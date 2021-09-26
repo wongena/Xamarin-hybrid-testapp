9be57196182aa4399ec534a7118bcf450fd50249
Wout Ongena <woutongena@gmail.com>
2021-09-26 11:54:24 +0200

    Added initial testapp

---
+++ App/XamarinTestApp/XamarinTestApp/XamarinTestApp/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using XamarinTestApp.ViewModels;
+using XamarinTestApp.Views;
+
+namespace XamarinTestApp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
